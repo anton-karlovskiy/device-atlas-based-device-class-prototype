387140699aae9c3af99932f71d2ed28204ba5df5
anton-karlovskiy <antonkarlovskiy@outlook.com>
2019-07-23 15:58:44 +0300

    added node server with proxy

---
+++ server.js
@@ -0,0 +1,23 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+app.disable('x-powered-by');
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+  return res.send('pong');
+});
+ 
+// need to declare a "catch all" route on your express server 
+// that captures all page requests and directs them to the client
+// the react-router do the route part
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.listen(
+    process.env.PORT || 5000,
+    function () {
+      console.log(`Frontend start on http://localhost:5000`);
+    }
+);
